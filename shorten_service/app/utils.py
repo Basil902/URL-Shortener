ec215249d5cc696fc54a34c2aad5d11e609e7ae6
Basil902 <Basil902@users.noreply.github.com>
2025-06-09 22:57:31 +0200

    Add function for shortening links

---
+++ shorten_service/app/utils.py
@@ -0,0 +1,13 @@
+import random as rand
+
+def generate_link():
+
+    chars = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+    chars = rand.sample(chars, 6)
+    code = "/" + "".join(chars)
+
+    link = "http://localhost:5000" + code
+
+    # returns both the whole link and the code alone
+    return {"link": link,
+            "link_code": code}
